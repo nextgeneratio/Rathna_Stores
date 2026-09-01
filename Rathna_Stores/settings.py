fb42cc6b56381347a090f438c8bfc4b9deff6205
nextgeneratio <currstud23123@gmail.com>
2026-09-01 16:19:36 +0530

    Update ALLOWED_HOSTS in settings.py for local development

--- Rathna_Stores/settings.py
+++ Rathna_Stores/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-9a=k-f6g@yc@#%_vvrj@q%5lf#gvayc9tp4z7-iw&8h+8t7dgr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '[::1]']
 
 
 # Application definition
